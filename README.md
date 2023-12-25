7b77f79f18f2f9e404eda4485bc94e31af3e95ac
Huy Vu <huyvu@huyvu-2.local>
2023-12-25 16:58:48 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning C++ with SFML and Game Development
+A free time hobby to gain more knowledge about lower system. These exercises are following this course: https://www.youtube.com/playlist?list=PL_xRyXins848nDj2v-TJYahzvs-XW9sVV
---
+++ main.cpp
@@ -0,0 +1,106 @@
+#include "SFML/Graphics.hpp"
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define W_HEIGHT 720
+#define W_WIDTH 1200
+#define FPS 60
+#define GAME_NAME "Back Office"
+
+class MyCircle : public sf::CircleShape {
+public:
+  float xVelocity;
+  float yVelocity;
+
+  MyCircle(float radius, float initialX, float initialY, float xVelocity,
+           float yVelocity, int rColor, int gColor, int bColor)
+      : CircleShape(radius), xVelocity(xVelocity), yVelocity(yVelocity) {
+    this->setPosition(initialX, initialY);
+    this->setFillColor(sf::Color(rColor, gColor, bColor));
+  }
+
+  void setBounce() {
+    float xCircle = this->getPosition().x;
+    float yCircle = this->getPosition().y;
+
+    if (xCircle <= 0.0f || xCircle + this->getRadius() * 2 >= W_WIDTH) {
+      this->xVelocity *= -1.0f;
+    }
+
+    if (yCircle <= 0.0f || yCircle + this->getRadius() * 2 >= W_HEIGHT) {
+      this->yVelocity *= -1.0f;
+    }
+
+    this->setPosition(xCircle + this->xVelocity, yCircle + this->yVelocity);
+  }
+};
+
+std::vector<std::string> splitString(const std::string &targetString,
+                                     const char &delimiter) {
+  std::vector<std::string> substrings;
+
+  long currentMatchedPos = 0;
+  std::string token;
+
+  std::size_t found;
+  while ((found = targetString.find(delimiter, currentMatchedPos)) !=
+         std::string::npos) {
+    token = targetString.substr(currentMatchedPos, found - currentMatchedPos);
+    substrings.push_back(token);
+    currentMatchedPos = 0;
+    currentMatchedPos += found + 1;
+  }
+
+  substrings.push_back(targetString.substr(currentMatchedPos));
+
+  return substrings;
+}
+
+std::vector<MyCircle> readConfigFile(const std::string &filePath) {
+  std::vector<MyCircle> shapes;
+
+  std::ifstream configFile(filePath);
+
+  std::string shapeConfig;
+  while (std::getline(configFile, shapeConfig)) {
+    std::vector<std::string> configs = splitString(shapeConfig, ' ');
+    MyCircle circle(std::stof(configs[1]), std::stof(configs[2]),
+                    std::stof(configs[3]), std::stof(configs[4]),
+                    std::stof(configs[5]), std::stof(configs[6]),
+                    std::stof(configs[7]), std::stof(configs[8]));
+    shapes.push_back(circle);
+  }
+  configFile.close();
+  return shapes;
+}
+
+int main(int argc, char *argv[]) {
+  sf::RenderWindow window(sf::VideoMode(W_WIDTH, W_HEIGHT), GAME_NAME);
+  window.setFramerateLimit(FPS);
+
+  std::vector<MyCircle> circles = readConfigFile("config.txt");
+  std::cout << "Objects loaded: " << circles.size() << std::endl;
+
+  while (window.isOpen()) {
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+
+    for (MyCircle &circle : circles) {
+      circle.setBounce();
+      window.draw(circle);
+    }
+
+    window.display();
+  }
+
+  return 0;
+}
